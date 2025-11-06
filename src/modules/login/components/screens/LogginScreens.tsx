9f8354de6153112f681137fddc76d1fd5d307cfc
Alisson Andrade <136262192+AlissonAndrade@users.noreply.github.com>
2025-11-05 22:50:45 -0300

    Update LogginScreens.tsx

--- src/modules/login/components/screens/LogginScreens.tsx
+++ src/modules/login/components/screens/LogginScreens.tsx
@@ -13,7 +13,7 @@ const LogginScreens = () => {
     return (
         
             <div>
-                <BackgroundImage typeof="img" src="vendas-online/public/background.png"/>
+                <BackgroundImage typeof="imgage" src="vendas-online/public/background.png"/>
                 <Body/>
                 <ContentLoggin>
                     <LimitLoggin>
